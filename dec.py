165721a616d065b58798dcc1be316f67610ef546
unknown <dalmq4806@gmail.comgit config --global user .name dalalgit config --global user.email dalmq4806@gmail.com>
2023-01-17 00:12:10 +0300

    lap.dec

---
+++ dec.py
@@ -0,0 +1,23 @@
+book_number = {"Amal": '0568323222', "Mohammed":'0522222232', "Khadijah": '0532335983', "Abdullah": '0545341144', "Rawan":'0545534556', "Faisal": '0560664566', "Layla": '0567917077'}
+
+def getOwnerPhone(phoneNumber) :
+    if len(str(phoneNumber)) == 10 and str(phoneNumber).isdigit():
+       for element in book_number:
+            if book_number[element] == phoneNumber:
+                 return element
+            else:
+                sorry = "Sorry,the number is not found"
+                return sorry
+    else:
+        return "This is invalid number"
+
+
+phoneNumber = input('please enter phone number : ')
+print(getOwnerPhone(phoneNumber))
+
+def rearranges(numberList: list) -> list:
+    listnu = [2, 3, 5, 6, 0, 4, 1]
+    numberList = [numberList[i] for i in listnu]
+    return numberList
+
+print(rearranges([5, 0, 34, 9, 0, 13, 8]))
