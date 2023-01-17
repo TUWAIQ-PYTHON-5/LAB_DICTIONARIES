aea2dd5bc321bd1f65259eb7e28cef9732cd8025
محمد الزيداني <zidani2030@gmail.com>
2023-01-17 09:35:24 +0300

    lab

---
+++ dects.py
@@ -0,0 +1,8 @@
+phone_book = {
+    "Amal":  568323222, "Mohammed":522222232 ,
+     "Khadijah":532335983 ,"Abdullah":545341144 ,
+     "Rawan":545534556, "Faisal":560664566,"Layla":567917077}
+
+print(phone_book)
+
+
