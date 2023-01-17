e1c9204e58418d2f17bc696fb07adbe956940ba8
raghad nasser <Ra9g77@gmail.com>
2023-01-17 18:53:05 +0300

    lab dictionaries sloved 3

--- lab dict.py
+++ lab dict.py
@@ -17,37 +17,19 @@ number=str(input("Enter the number:"))
 
 phone_book1={"0568323222":"Amal","0522222232":"Mohammed","0532335983":"Khadijah","0545341144":"Abdullah","0545534556":"Rawan","0560664566":"Faisal","0567917077":"Layla"}
  
-number_2=int(number) 
-if number_2>10 and number_2<10:
-     print ("This is invalid number")
+ 
+
+
+if not number.isdigit() or len(number)!=10 :
+    print ("This is invalid number") 
+elif phone_book1.get(number)==None :
+        print("Sorry, the number is not found")
+else  :
+    print( phone_book1.get(number))
 
 
 
 
-
-while number =="0568323222":
-  print("the name of the owner is Amal")
-  break
-while number =="0522222232":
-  print("the name of the owner is Mohammed")
-  break
-while number =="0532335983":
-  print("the name of the owner is Khadijah")
-  break
-while number =="0545341144":
-  print("the name of the owner is Abdullah")
-  break
-while number =="0545534556":
-  print("the name of the owner is Rawan")
-  break
-while number =="0560664566":
-  print("the name of the owner is Faisal")
-  break
-while number =="0567917077":
-  print("the name of the owner is Layla")
-  break
-else:
-  print( "Sorry, the number is not found")
 
 
 
