475c50d9b33fe3724db49c86ec7d6965532b985b
sultan <sultan_33_886@outlook.sa>
2023-01-17 08:43:02 +0300

    lab_DICIONNARIS

---
+++ dictonaries lab.py
@@ -0,0 +1,37 @@
+phone_book= {"Amal":568323222,"Mohammed" :568323222,"Khadijah":532335983,"Abdullah":545341144,"Rawan":545534556,"Faisal":560664566,"Layla":567917077}
+for x, y in phone_book.items():
+    print(y)
+else:
+    print("Sorry,the number is not found")
+
+
+
+
+for x,y in phone_book.items():
+    if y== phone_book:
+        print(len[y])
+    else:
+     print("This is invalid number")
+
+
+
+for x,y in phone_book.items():
+    if y== y:
+        print(y)
+    else:        
+     print("This is invalid number")
+
+
+
+
+def rearranges(ls:list):
+    for ls in range(0,7,2):
+     print(ls)
+rearranges(5,0,43,9,0,13,8)
+        
+    
+
+
+
+
+
